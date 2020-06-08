28cdaff357d576ed48ab9feffb99e1ece4e5e8d3
Michael Simard <msimard8@Gmail.com>
2020-06-08 08:43:09 -0500

    update cron to download only 3 hours rather than every minute

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # Start the run once job.
 echo "Docker container has been started"
 # Setup a cron schedule
-echo "* * * * * /run.sh >> /var/log/cron.log 2>&1
+echo "* */3 * * * /run.sh >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 crontab scheduler.txt
 cron -f
