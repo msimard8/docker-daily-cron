36991df179ac3b4c2d87c93a40256a9ccf45b737
Michael Simard <michael@michaelsimard.com>
2020-08-11 21:07:40 -0500

    update cron scrypt

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 # Start the run once job.
 echo "Docker container has been started"
 # Setup a cron schedule
-echo "* */3 * * * /run.sh >> /var/log/cron.log 2>&1
-# This extra line makes it a valid cron" > scheduler.txt
-crontab scheduler.txt
+printf "* */3 * * * /run.sh >> /var/log/cron.log 2>&1\n# This extra line makes it a valid cron" > scheduler.txt
+
 cron -f
+
+
+
+
